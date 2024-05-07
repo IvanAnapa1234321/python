66a9c5c4094ca01b74feb6f19939db9d8e52babf
Your Name <you@example.com>
2024-05-07 18:26:17 +0300

    first commit 123

---
+++ dz.py
@@ -0,0 +1,19 @@
+int_Z=1
+float_Q=11.7
+long_w="qwerty_привет"
+a = ['п', 'р', 'и', 'в', 'е', 'т']
+
+print(int_Z)
+print(long_w)
+print(float_Q)
+print(a)
+S=2
+while S<20:
+ print(S)
+ S=S*2
+ C=15
+ C=C+1
+ if C<20:
+  print(C)
+
+ 
